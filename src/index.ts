44f6350991d22d52d1fd588c3cc18162d8ca87cf
Edwin Cosme <adriancosme15@gmail.com>
2023-05-22 21:34:26 -0500

    feat: add provider

---
+++ src/index.ts
@@ -0,0 +1,80 @@
+// import type provider from next-auth package
+import type { OAuthUserConfig, OAuthConfig } from "next-auth/providers/oauth";
+
+export interface InfojobsCandidate extends Record<string, any> {
+    id: number;
+    email: string;
+    photo: string;
+    name: string;
+}
+
+interface AdditionalConfig {
+    infojobs_scopes: string;
+    redirect_uri: string;
+}
+
+const AUTHORIZATION_URL = "https://www.infojobs.net/api/oauth/user-authorize/index.xhtml"
+const TOKEN_URL = "https://www.infojobs.net/oauth/authorize";
+export default function InfojobsProvider<P extends InfojobsCandidate>(options: OAuthUserConfig<P> & AdditionalConfig): OAuthConfig<P> {
+    return {
+        id: 'infojobs',
+        name: 'Infojobs',
+        type: 'oauth',
+        version: '2.0',
+        checks: ['none'],
+        authorization: {
+            url: AUTHORIZATION_URL,
+            params: {
+                scope: options.infojobs_scopes,
+                redirect_uri: options.redirect_uri,
+                response_type: "code",
+            },
+        },
+        token: {
+            url: TOKEN_URL,
+            async request({ params }) {
+                const tokenUrl = new URL(TOKEN_URL ?? '');
+                tokenUrl.searchParams.append('grant_type', 'authorization_code');
+                tokenUrl.searchParams.append('code', params.code ?? '');
+                tokenUrl.searchParams.append('redirect_uri', `${options.redirect_uri}` ?? '');
+                tokenUrl.searchParams.append('client_id', options.clientId ?? '');
+                tokenUrl.searchParams.append('client_secret', options.clientSecret ?? '');
+                const response = await fetch(tokenUrl.toString(), {
+                    method: 'POST',
+                })
+                const tokens = await response.json();
+                return {
+                    tokens
+                }
+            }
+        },
+        userinfo: {
+            async request({ tokens }) {
+                const basicToken = `Basic ${Buffer.from(`${options.clientId}:${options.clientSecret}`).toString('base64')}`;
+                const bearerToken = `Bearer ${tokens.access_token}`;
+                const response = await fetch("https://api.infojobs.net/api/6/candidate", {
+                    headers: {
+                        Authorization: `${basicToken},${bearerToken}`,
+                    },
+                });
+                const profile = await response.json();
+                return {
+                    id: profile.id,
+                    email: profile.email,
+                    image: profile.photo,
+                    name: profile.name,
+                    sub: profile.id,
+                }
+            }
+        },
+        profile(profile) {
+            return {
+                id: profile.id.toString(),
+                name: profile.name,
+                email: profile.email,
+                image: profile.photo,
+            }
+        },
+        options
+    }
+}
